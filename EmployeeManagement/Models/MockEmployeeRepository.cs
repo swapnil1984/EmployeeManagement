67e4a64fc19bccf238d0fbe0033d1534a6368e36
Swapnil Sule <sule.swapnil@yahoo.com>
2021-05-30 22:03:50 -0300

    scrambled the email addresses

--- EmployeeManagement/Models/MockEmployeeRepository.cs
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -14,9 +14,9 @@ namespace EmployeeManagement.Models
         {
             _employeeList = new List<Employee>()
             {
-                new Employee() { Id = 1, Name = "Swapnil", Department = "Engineering", Email = "sule.swapnil@yahoo.com" },
+                new Employee() { Id = 1, Name = "Swapnil", Department = "Engineering", Email = "blah.blah@yahoo1.com" },
                 new Employee() { Id = 2, Name = "John", Department = "HR", Email = "john.doe@gmail.com" },
-                new Employee() { Id = 3, Name = "Ashlee", Department = "Sales", Email = "ashlee.blah@hotmail.com" }
+                new Employee() { Id = 3, Name = "Ashlee", Department = "Sales", Email = "blah.blah1@hotmail.com" }
             };
         }
         public Employee GetEmployee(int Id)
