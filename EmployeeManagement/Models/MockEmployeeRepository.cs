b68b51bc8f5ca72252833e924388d798caa113fa
Swapnil Sule <sule.swapnil@yahoo.com>
2021-05-30 22:02:12 -0300

    Add project files.

---
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    // Its important to use Interface instead of direct classes to allow dependency injection
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Swapnil", Department = "Engineering", Email = "sule.swapnil@yahoo.com" },
+                new Employee() { Id = 2, Name = "John", Department = "HR", Email = "john.doe@gmail.com" },
+                new Employee() { Id = 3, Name = "Ashlee", Department = "Sales", Email = "ashlee.blah@hotmail.com" }
+            };
+        }
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
