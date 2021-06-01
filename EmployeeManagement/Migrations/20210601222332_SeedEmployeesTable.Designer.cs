c78ae27fa4bd21aab45d997967c55f16c3ff8424
Swapnil Sule <sule.swapnil@yahoo.com>
2021-06-01 19:34:36 -0300

    Added code and migration to Seed the data

--- EmployeeManagement/Migrations/20210601222332_SeedEmployeesTable.Designer.cs
+++ EmployeeManagement/Migrations/20210601222332_SeedEmployeesTable.Designer.cs
@@ -3,14 +3,16 @@ using EmployeeManagement.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace EmployeeManagement.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210601222332_SeedEmployeesTable")]
+    partial class SeedEmployeesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -46,15 +48,8 @@ namespace EmployeeManagement.Migrations
                         {
                             Id = 1,
                             Department = 3,
-                            Email = "mary@yahoo.com",
-                            Name = "Mary"
-                        },
-                        new
-                        {
-                            Id = 2,
-                            Department = 1,
-                            Email = "john@yahoo.com",
-                            Name = "John"
+                            Email = "mark@yahoo.com",
+                            Name = "Mark"
                         });
                 });
 #pragma warning restore 612, 618
--- EmployeeManagement/Models/AppDbContext.cs
+++ EmployeeManagement/Models/AppDbContext.cs
@@ -15,5 +15,10 @@ namespace EmployeeManagement.Models
         }
 
         public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Seed();
+        }
     }
 }
